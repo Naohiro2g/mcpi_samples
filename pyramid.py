584851f7ff901f03cabc1a18612f63831c3f6957
naohiro2g <naohiro2g@code2create.club>
2019-11-27 13:50:21 +0900

    update

--- pyramid.py
+++ pyramid.py
@@ -13,13 +13,14 @@ def setPyramid(x=0, z=0, size=3, y=0, blockTypeId=41, blockData=0):
         z += 1
         size -= 2
         y += 1
-#        sleep(0.025)
+        sleep(0.025)
 
 
 def woolRainbow(x, z, size=7, n=64):
     for i in range(n):
-        setPyramid2(x=x, z=z, size=size, blockTypeId=35, blockData=(i % 16))
+        setPyramid(x=x, z=z, size=size, blockTypeId=35, blockData=(i % 16))
         sleep(0.25)
+
 
 def clearField():
     mc.setBlocks(-50, 0, -50, 50, 19, 50, 0)
@@ -29,6 +30,6 @@ clearField()
 sleep(1)
 setPyramid(x=0, z=0, size=27, blockTypeId=41, blockData=0)
 sleep(2)
-setPyramid2(x=15, z=15, size=27, blockTypeId=42, blockData=0)
+setPyramid(x=15, z=15, size=27, blockTypeId=42, blockData=0)
 sleep(2)
 woolRainbow(x=0, z=20, size=27, n=513)
