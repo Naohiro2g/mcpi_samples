7df851e8b5565512b9a2225be6f11341bb3b8521
naohiro2g <587586+Naohiro2g@users.noreply.github.com>
2022-06-26 06:06:32 +0900

    made main part, bug fix for using multiple cranes

--- craw.py
+++ craw.py
@@ -5,31 +5,30 @@ from time import sleep
 
 # mc = Minecraft.create("primbell002.local")
 mc = Minecraft.create()
-mc.postToChat("Crane - UFO catcher")
 
 
 class Crane():
     def __init__(self, cranePos):
         self.pos = cranePos
         self.isOpen = True
-        Crane.draw(self)
+        Crane.clear(self)
 
     def clear(self):
         Crane.draw(self, clear=True)
 
-    def openCraw(self, pose=0.5):
+    def openCraw(self, pause=0.5):
         if not self.isOpen:
             Crane.draw(self, clear=True)
             self.isOpen = True
             Crane.draw(self, clear=False)
-        sleep(pose)
+        sleep(pause)
 
-    def closeCraw(self, pose=0.5):
+    def closeCraw(self, pause=0.5):
         if self.isOpen:
             Crane.draw(self, clear=True)
             self.isOpen = False
             Crane.draw(self, clear=False)
-        sleep(pose)
+        sleep(pause)
 
     def draw(self, blockTypeId=41, blockData=0, clear=False):
         x, y, z = self.pos.x, self.pos.y, self.pos.z
@@ -62,55 +61,63 @@ class Crane():
             mc.setBlocks(x,     y - 4, z + 3, x,     y - 4, z + 1, blockTypeId, blockData)
             mc.setBlocks(x,     y - 4, z - 3, x,     y - 4, z - 1, blockTypeId, blockData)
 
-    def move(self, dX=None, dY=None, dZ=None, speed=1, pose=0.5):
+    def move(self, dX=None, dY=None, dZ=None, speed=1, pause=0.5):
         step = 1
         if dX:
             if dX < 0:
                 step = -1
-            for i in range(cranePos.x, cranePos.x + dX, step):
-                cranePos.x = i
+            for _x in range(0, dX, step):
                 Crane.draw(self)
                 sleep(0.1)
                 Crane.draw(self, clear=True)
+                self.pos.x += step
         if dY:
             if dY < 0:
                 step = -1
-            for i in range(cranePos.y, cranePos.y + dY, step):
-                cranePos.y = i
+            for _y in range(0, dY, step):
                 Crane.draw(self)
                 sleep(0.1)
                 Crane.draw(self, clear=True)
+                self.pos.y += step
         if dZ:
             if dZ < 0:
                 step = -1
-            for i in range(cranePos.z, cranePos.z + dZ, step):
-                cranePos.z = i
+            for _z in range(0, dZ, step):
                 Crane.draw(self)
                 sleep(0.1)
                 Crane.draw(self, clear=True)
+                self.pos.z += step
         Crane.draw(self)
-        sleep(pose)
+        sleep(pause)
 
 
-# main
+# for Java Edition
+# crane1 = Crane(Vec3(-10, 90, 5))
+# crane2 = Crane(Vec3(10, 90, 20))
+crane1 = Crane(Vec3(-10, 25, 5))
+crane2 = Crane(Vec3(10, 25, 20))
+mc.postToChat("Crane module loaded")
 
-# mc.setBlocks(-50, 0, -50,   50, 40, 50,  0)
+if __name__ == "__main__":
 
-cranePos = Vec3(-10, 25, 5)
-crane1 = Crane(cranePos)
-crane1.move(pose=1)
+    mc.postToChat("Crane - UFO catcher")
 
-for j in range(2):
-    crane1.closeCraw(pose=1)
-    crane1.openCraw(pose=1)
-crane1.move(dX=40)
-crane1.move(dZ=10)
+    # mc.setBlocks(-50, 0, -50,   50, 40, 50,  0)
+    mc.setBlocks(-50, 70, -50,   50, 120, 50,  0)
 
-crane1.move(dY=-10)
-crane1.closeCraw(pose=2)
-crane1.move(dY=10)
-crane1.move(dX=-20)
+    crane1.move(pause=1)
 
-crane1.openCraw(pose=3)
+    for _ in range(2):
+        crane1.closeCraw(pause=1)
+        crane1.openCraw(pause=1)
+    crane1.move(dX=40)
+    crane1.move(dZ=10)
 
-crane1.clear()
+    crane1.move(dY=-10)
+    crane1.closeCraw(pause=2)
+    crane1.move(dY=10)
+    crane1.move(dX=-20)
+
+    crane1.openCraw(pause=3)
+
+    crane1.clear()
